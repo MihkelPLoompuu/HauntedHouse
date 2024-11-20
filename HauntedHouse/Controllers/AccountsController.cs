48401d9ff1543971c54333640e87f5929e394088
Opilane TTHK <opilane@tthk.ee>
2024-11-20 15:04:37 +0200

    Add new controller and view

---
+++ HauntedHouse/Controllers/AccountsController.cs
@@ -0,0 +1,57 @@
+﻿using HauntedHouse.Core.Domain;
+using HauntedHouse.Data;
+using HauntedHouse.Models.Accounts;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HauntedHouse.Controllers
+{
+    public class AccountsController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _SignInManager;
+        private readonly HunterContext _context;
+
+        public AccountsController(UserManager<ApplicationUser> userManager,SignInManager<ApplicationUser> signInManager, HunterContext context)
+        {
+            _userManager = userManager;
+            _SignInManager = signInManager;
+            _context = context; 
+        }
+        [HttpGet]
+        public async Task<IActionResult> AddPassword()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            var userHasPassword = await _userManager.HasPasswordAsync(user);
+            if (userHasPassword)
+            {
+                RedirectToAction("ChangePassword");
+            }
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> AddPassword(AddPasswordViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.GetUserAsync(User);
+                var result = await _userManager.AddPasswordAsync(user, model.NewPassword);
+                if (!result.Succeeded)
+                {
+                    foreach(var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                    return View();
+                }
+                await _SignInManager.RefreshSignInAsync(user);
+                return View("AddPasswordConfirmation");
+            }
+            return View(model);
+        }
+
+    }
+
+}
