68d887d3fa5483f89d3c084672211409d6e2c708
Opilane TTHK <opilane@tthk.ee>
2024-11-20 14:12:20 +0200

    Add AccountDto and Classes in Account

---
+++ HauntedHouse.Core/Dto/AccountsDtos/ApplicationUserDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HauntedHouse.Core.Dto.AccountsDtos
+{
+    public class ApplicationUserDto
+    {
+        public string City {  get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public Guid? AssociatedPlayerProfile {  get; set; }
+    }
+}
