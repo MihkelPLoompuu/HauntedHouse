132e3883cf0dc11b609a933c2e97c008fed3d37e
Opilane TTHK <opilane@tthk.ee>
2024-11-20 15:20:48 +0200

    Pean laadima uue Pagi

---
+++ HauntedHouse.ApplicationServices/Services/AccountsServices.cs
@@ -0,0 +1,23 @@
+﻿using HauntedHouse.Core.Domain;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HauntedHouse.ApplicationServices.Services
+{
+    public class AccountsServices : IAccountServices
+    {
+        private readonly UserManager<ApplicationUser> _userManager; // lae uus pag
+        private readonly SignInManager<ApplicationUser> _SignInManager;
+
+        public AccountsServices(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _SignInManager = signInManager;
+        }
+    }
+}
