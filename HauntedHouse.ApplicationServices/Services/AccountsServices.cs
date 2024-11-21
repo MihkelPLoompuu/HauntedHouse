06d387a24c5d73d9189218782de322c3efb8bc0b
MihkelPLoompuu <158562003+MihkelPLoompuu@users.noreply.github.com>
2024-11-21 09:21:15 +0200

    tegin korda too

--- HauntedHouse.ApplicationServices/Services/AccountsServices.cs
+++ HauntedHouse.ApplicationServices/Services/AccountsServices.cs
@@ -1,6 +1,5 @@
 ﻿using HauntedHouse.Core.Domain;
 using HauntedHouse.Core.ServiceInterface;
-using Microsoft.AspNet.Identity;
 using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
