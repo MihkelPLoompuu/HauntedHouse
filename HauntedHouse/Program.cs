c3369bb63e53046112272182ac02f578f34c0775
Opilane TTHK <opilane@tthk.ee>
2024-11-29 12:48:37 +0200

    saime tehtud

--- HauntedHouse/Program.cs
+++ HauntedHouse/Program.cs
@@ -1,6 +1,9 @@
 using HauntedHouse.ApplicationServices.Services;
+using HauntedHouse.Core.Domain;
 using HauntedHouse.Core.ServiceInterface;
 using HauntedHouse.Data;
+using HauntedHouse.Security;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace HauntedHouse
@@ -15,8 +18,31 @@ namespace HauntedHouse
             builder.Services.AddControllersWithViews();
             builder.Services.AddScoped<IHuntersServices, HuntersServices>();
             builder.Services.AddScoped<IFileServices, FileServices>();
+            builder.Services.AddScoped<IAccountsServices, AccountsServices>();
             builder.Services.AddDbContext<HauntedHouseContext>(
             options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+            builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+            {
+                options.SignIn.RequireConfirmedAccount = true;
+                options.Password.RequiredLength = 3;
+
+                options.Tokens.EmailConfirmationTokenProvider = "CustomEmailConfirmation";
+                options.Lockout.MaxFailedAccessAttempts = 3;
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+            })
+            .AddEntityFrameworkStores<HauntedHouseContext>()
+            .AddDefaultTokenProviders()
+            .AddTokenProvider<DataProtectorTokenProvider<ApplicationUser>>("CustomEmailConfirmation")
+            .AddDefaultUI();
+
+            builder.Services.Configure<DataProtectionTokenProviderOptions>
+                (options => options.TokenLifespan = TimeSpan.FromHours(5)
+                );
+            builder.Services.Configure<CustomEmailConfirmationTokenProviderOptions>(
+                options => options.TokenLifespan = TimeSpan.FromDays(3)
+                );
+
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
---
+++ HauntedHouse/Security/CustomEmailConfirmationTokenProviderOptions.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace HauntedHouse.Security
+{
+    public class CustomEmailConfirmationTokenProviderOptions : DataProtectionTokenProviderOptions
+    {
+    }
+}
