7e76d3fd3c20a76e19ab4d975f127076532b34b0
Opilane TTHK <opilane@tthk.ee>
2024-12-04 15:02:06 +0200

    Emails

---
+++ HauntedHouse/Controllers/EmailsController.cs
@@ -0,0 +1,33 @@
+﻿using HauntedHouse.Core.Dto;
+using HauntedHouse.Models.Emails;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HauntedHouse.Controllers
+{
+    public class EmailsController : Controller
+    {
+        private readonly IEmailServices _emailsServices;
+        public EmailsController(IEmailServices emailsServices)
+        {
+            _emailsServices = emailsServices;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SendEmail(EmailViewModel viewModel) 
+        {
+            var dto = new EmailDto()
+            {
+                To = viewModel.To,
+                Subject = viewModel.Subject,
+                Body = viewModel.Body,
+            };
+            _emailsServices.SendEmail(dto);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ HauntedHouse/Models/Emails/EmailViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace HauntedHouse.Models.Emails
+{
+    public class EmailViewModel
+    {
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+}
