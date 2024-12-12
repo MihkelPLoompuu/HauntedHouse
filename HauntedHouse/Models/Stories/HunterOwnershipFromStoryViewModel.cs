fbd6ee583b2fb4f6a8c50b6cda8c5c38fc9a6493
Opilane TTHK <opilane@tthk.ee>
2024-12-12 15:22:29 +0200

    lisasin asju

---
+++ HauntedHouse/Models/Stories/HunterOwnershipFromStoryViewModel.cs
@@ -0,0 +1,13 @@
+﻿using HauntedHouse.Core.Dto;
+
+namespace HauntedHouse.Models.Stories
+{
+    public class HunterOwnershipFromStoryViewModel
+    {
+        public string PlayerProfileGUID { get; set; }
+
+        public string StoryGUID { get; set;}
+
+        public  HunterOwnershipDto addHunter {  get; set; }
+    }
+}
