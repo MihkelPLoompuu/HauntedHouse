061bfe5a05080295907475635bbc863cf77c9872
Opilane TTHK <opilane@tthk.ee>
2024-12-12 13:51:32 +0200

    Lisasin Player profile

---
+++ HauntedHouse.Core/Domain/HunterOwnership.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HauntedHouse.Core.Domain
+{
+    public class HunterOwnership
+    {
+        public Guid OwnershipID { get; set; }
+        public int HunterHealth { get; set; }
+        public int HunterXP { get; set; }
+        public int HunterXPNextLevel { get; set; }
+        public int HunterLevel { get; set; }
+        public HunterStatus HunterStatus { get; set; }
+        public int PrimaryAttackPower { get; set; }
+        public int SecondaryAttackPower { get; set; }
+        public int SpecialAttackPower { get; set; }
+        public DateTime OwnershipCreatedAt { get; set; }
+        public DateTime OwnershipUpdatedAt { get; set; }
+    }
+}
