42f34bffd354dea3d4c80cdf7d96efc6683050d1
MihkelPLoompuu <158562003+MihkelPLoompuu@users.noreply.github.com>
2025-01-05 14:05:51 +0200

    lisasin roome juurde

--- HauntedHouse/Controllers/BuildingsController.cs
+++ HauntedHouse/Controllers/BuildingsController.cs
@@ -1,12 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HauntedHouse.Core.ServiceInterface;
+using HauntedHouse.Data;
+using HauntedHouse.Models.Buildings;
+using HauntedHouse.Models.Rooms;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace HauntedHouse.Controllers
 {
     public class BuildingsController : Controller
     {
+        private readonly HauntedHouseContext _context;
+        public BuildingsController(HauntedHouseContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            var resultingInventory = _context.Buildings
+                           .OrderByDescending(y => y.BuildingType)
+                           .Select(x => new BuildingIndexViewModel
+                           {
+                               ID = x.ID,
+                               BuildingName = x.BuildingName,
+                               BuildingType = x.BuildingType,   
+                           });
+            return View(resultingInventory);
         }
     }
 }
