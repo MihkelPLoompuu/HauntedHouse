a17a6f7cff1275c3b17a91d7735cc320033dcff5
MihkelPLoompuu <158562003+MihkelPLoompuu@users.noreply.github.com>
2025-01-05 11:57:51 +0200

    tegin modelid ara

---
+++ HauntedHouse/Models/Buildings/BuildingIndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿using HauntedHouse.Core.Domain;
+
+namespace HauntedHouse.Models.Buildings
+{
+    public class BuildingIndexViewModel
+    {
+        public Guid ID { get; set; }
+        public string BuildingName { get; set; }
+        public BuildingType BuildingType { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
